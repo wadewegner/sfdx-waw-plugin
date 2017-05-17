6af3477d3dee177061810120b8786e4aa8c591cd
Wade Wegner <wade.wegner@gmail.com>
2017-05-16 20:07:53 -0700

    Refactored

--- commands/oss.js
+++ commands/oss.js
@@ -72,6 +72,8 @@ const files = require('../lib/files.js');
 
         const rawUrlManifestFolder = `https://raw.githubusercontent.com/${context.flags.repository}/${branch}`;
         const rawUrlManifest = `${rawUrlManifestFolder}/sfdx-oss-manifest.json`;
+        const rawApiUrl = `https://api.github.com/repos/${context.flags.repository}/git/trees/${branch}?recursive=1`;
+
 
         // check to ensure sfdx-oss-manifest.json exists   
         urlExists(rawUrlManifest, (err2, manifestExists) => {
@@ -84,6 +86,7 @@ const files = require('../lib/files.js');
 
             const manifestJson = JSON.parse(body);
             const version = manifestJson.version;
+            const sourceFolder = manifestJson.sourceFolder;
 
             // check version
             if (version === '1.0.0') {
@@ -95,6 +98,19 @@ const files = require('../lib/files.js');
                 console.log('Currently only sfdx source is supported'); // eslint-disable-line no-console
                 return;
               }
+
+              const manifestFolders = manifestJson.folders;
+
+              files.writeFolders(manifestFolders, targetPath, rawUrlManifestFolder, rawApiUrl, sourceFolder, (outputFiles) => {
+
+                console.log('Writing files for folders ...'); // eslint-disable-line no-console
+
+                for (let i = 0, len = outputFiles.length; i < len; i++) {
+                  console.log(`  ${outputFiles[i]}`); // eslint-disable-line no-console
+                }
+
+              });
+              
 
               // grab the files
               const manifestFiles = manifestJson.files;
